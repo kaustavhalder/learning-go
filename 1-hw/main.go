7e59ea66c22f24a275e954f478c7a6923cf8f9b5
kaustavhalder <khalder.08@gmail.com>
2023-01-28 23:10:43 -0600

    updated to hw folder

--- 1-hw/main.go
+++ 1-hw/main.go
@@ -5,10 +5,16 @@ import "fmt"
 // when declaring outside, need to use var, short declartion uses only within function body
 // declaration syntax = var {name} then type, unlike other prgramming languages
 // go can implicitly identify the type of the variable, if no type is mentioned during declaration
+// Golang types
+// strongly/static typed, so variables with declared type, cannot be used for other types
+
 var z int
 var name string = "VLAD"
-// Golang types
-// strongly/static typed, so variables with declared type, cannot be used for other types 
+
+// Creation of custom types in Golang
+var a int
+type hotrod int
+var b hotrod
 
 func foo() {
 	fmt.Println("this is from the foo functino")
@@ -19,10 +25,14 @@ func main() {
 	// declare and assign at the same time
 	// short declaration can only be used inside a function
 	x := 43
+	a = 51
 	fmt.Println("this is a test ")
 	foo()
 	fmt.Println("end of program")
 	x = 50
+	fmt.Println(a)
+	fmt.Println("b is being printed", b)
+	fmt.Printf("%T\n",b)
 	fmt.Println(x)
 	// during assignemant you can use expressions as well
 	y := 100 + 24
