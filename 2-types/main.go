3cd3c579d481bea4efa53a800bcbc9a4570c02bd
kaustavhalder <khalder.08@gmail.com>
2023-01-29 12:15:40 -0600

    types

--- 2-types/main.go
+++ 2-types/main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
-func main(){
+// Zero value for boolean is false
+
+var x bool
+
+func main() {
 	fmt.Println("this package is for types")
+	fmt.Println(x)
+	x = true
+	fmt.Println(x)
 }
