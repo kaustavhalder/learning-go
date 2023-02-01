57db245ee0d40c849eac6c5ff00b6f23ee4f8142
kaustavhalder <khalder.08@gmail.com>
2023-01-31 21:03:14 -0600

    added README file

--- 3-control-flow/main.go
+++ 3-control-flow/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("this section is for control flow")
+	fmt.Println("this is for control flow")
+	for i := 0; i < 10; i++ {
+		fmt.Println("Value of i is ", i)
+		for j := 0; j < 3; j++ {
+			fmt.Println("j is ", j)
+		}
+	}
 }
---
+++ README.md
@@ -0,0 +1,4 @@
+# Learning GO
+
+## Usage
+This repo is just a placeholder, while learning the Go Programming Language
