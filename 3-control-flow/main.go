5e86dae980b9180da3962d21e3a6898fd04fa3ef
kaustavhalder <khalder.08@gmail.com>
2023-02-01 22:26:57 -0600

    updated if

--- 3-control-flow/main.go
+++ 3-control-flow/main.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("this is for control flow")
-	for i := 0; i < 10; i++ {
-		fmt.Println("Value of i is ", i)
-		for j := 0; j < 3; j++ {
-			fmt.Println("j is ", j)
+	fmt.Println("this is a test")
+	for i := 0; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println("EVEN:", i)
+		} else {
+			fmt.Println("ODD:", i)
 		}
 	}
 }
