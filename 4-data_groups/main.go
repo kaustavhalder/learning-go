850398c085b76a150e92825c5508dc33c77cb533
kaustavhalder <khalder.08@gmail.com>
2023-02-02 20:59:24 -0600

    added maps

--- 4-data_groups/main.go
+++ 4-data_groups/main.go
@@ -32,4 +32,27 @@ func main() {
 	y := []int{98, 87, 65, 53}
 	x = append(x, y...) // this is sort of an unpacking operation being done on the slice and or any datatypes
 	fmt.Println("this is the slice after appending another slice", x)
+	// Delete from a slice
+	x = append(x[:2], x[4:]...)
+	fmt.Println("slice after the delete operation", x)
+	// Make - using this to crate a slice make([]int, 10, 1000)
+
+	// Maps
+	m := map[string]int{
+		"James": 32,
+		"Novak": 27,
+		"Roger": 33,
+	}
+	fmt.Println(m)
+	fmt.Println(m["James"])
+	fmt.Println(m["Roger"]) // Zero value is returned
+	v, ok := m["Roger"]
+	fmt.Println(v)
+	fmt.Println(ok)
+	if v, ok := m["Roger"]; ok {
+		fmt.Print("PRint ", v)
+	}
+	m["Andrei"] = 44
+	fmt.Println(m)
+
 }
