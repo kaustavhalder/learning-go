3b3acaf7e81b8687fbf05978f65f276e4a748727
kaustavhalder <khalder.08@gmail.com>
2023-02-02 18:34:22 -0600

    added data groups

--- 3-control-flow/main.go
+++ 3-control-flow/main.go
@@ -3,12 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("this is a test")
-	for i := 0; i <= 10; i++ {
-		if i%2 == 0 {
-			fmt.Println("EVEN:", i)
-		} else {
-			fmt.Println("ODD:", i)
-		}
+	// for i := 0; i <= 10; i++ {
+	// 	if i%2 == 0 {
+	// 		fmt.Println("EVEN:", i)
+	// 	} else {
+	// 		fmt.Println("ODD:", i)
+	// 	}
+	// }
+	switch {
+	case false:
+		fmt.Println("this is false")
+	case true:
+		fmt.Println("this is true")
+	default:
+		fmt.Println("this is default value")
 	}
 }
---
+++ 4-data_groups/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("this section is for arrays and slices")
+	var x [5]int
+	fmt.Println(x)
+	x[0] = 9
+	fmt.Println(x)
+	fmt.Println(len(x))
+}
