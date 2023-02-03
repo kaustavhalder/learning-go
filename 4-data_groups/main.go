2f6d4a3a81a5a05d1178c5c87b1b2edb2b778853
kaustavhalder <khalder.08@gmail.com>
2023-02-02 18:35:11 -0600

    updated

--- 4-data_groups/main.go
+++ 4-data_groups/main.go
@@ -6,9 +6,10 @@ import (
 
 func main() {
 	fmt.Println("this section is for arrays and slices")
-	var x [5]int
-	fmt.Println(x)
-	x[0] = 9
-	fmt.Println(x)
-	fmt.Println(len(x))
+	// var x [5]int
+	// fmt.Println(x)
+	// x[0] = 9
+	// fmt.Println(x)
+	// fmt.Println(len(x))
+
 }
