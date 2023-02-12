b90dcb8749aef2bbc3b9db4791ce37e88bebe71a
kaustavhalder <khalder.08@gmail.com>
2023-02-12 11:47:36 -0600

    init structs

--- 5-structs/main.go
+++ 5-structs/main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
-func  main(){
+type person struct {
+	first string
+	last  string
+}
+
+func main() {
 	fmt.Println("this section is for structs")
+	p1 := person{
+		first: "james",
+		last:  "bond",
+	}
+	fmt.Println(p1)
+	fmt.Println(p1.first)
+	fmt.Println(p1.last)
 }
