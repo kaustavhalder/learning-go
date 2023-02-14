cdfbf56c0712e2d1e1b62f27d184dfa0e3077cda
kaustavhalder <khalder.08@gmail.com>
2023-02-13 22:27:32 -0600

    added embedded types

--- 5-structs/main.go
+++ 5-structs/main.go
@@ -7,6 +7,14 @@ type person struct {
 	last  string
 }
 
+// Embedded types
+// Similar to other programming paradigms in
+
+type secretAgent struct {
+	person
+	ltk bool
+}
+
 func main() {
 	fmt.Println("this section is for structs")
 	p1 := person{
@@ -16,4 +24,14 @@ func main() {
 	fmt.Println(p1)
 	fmt.Println(p1.first)
 	fmt.Println(p1.last)
+
+	sAgent := secretAgent{
+		person: person{
+			first: "Vlad",
+			last:  "Funky",
+		},
+		ltk: true,
+	}
+	fmt.Println(sAgent)
+
 }
