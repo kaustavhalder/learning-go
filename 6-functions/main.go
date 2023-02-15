29c55495643da9d153ba18cd3e1fe5674787ea96
kaustavhalder <khalder.08@gmail.com>
2023-02-14 18:34:43 -0600

    added placeholder functions

--- 6-functions/main.go
+++ 6-functions/main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+// Functions in GO are always pass by value and not reference 
+// GO is typically not oop oriented, but there are oop features 
+
+func foo(){
+	fmt.Println("this is called from foo")
+}
+
 func main(){
 	fmt.Println("this is for functions")
+	foo()
 }
+
