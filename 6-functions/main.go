dab1793a9fd6c659e074aac8757e34a48643bf6c
kaustavhalder <khalder.08@gmail.com>
2023-02-15 22:01:46 -0600

    vairadic parameters

--- 6-functions/main.go
+++ 6-functions/main.go
@@ -5,33 +5,42 @@ import "fmt"
 // Functions in GO are always pass by value and not reference
 // GO is typically not oop oriented, but there are oop features
 
-func foo() {
-	fmt.Println("this is called from foo")
+// func foo() {
+// 	fmt.Println("this is called from foo")
+// }
+
+// // Paramters defined below
+// func bar(s string) {
+// 	fmt.Println("hello", s)
+// }
+
+// // Retrun types are defined below
+// func woo(s string) string {
+// 	return "Wooo"
+// }
+
+// // Retrun types are defined below
+// // Different return type , match the funtion signature
+// func test(fn string, ln string) (string, bool) {
+// 	fmt.Println("this is from te test fucntion")
+// 	return fn, true
+// }
+
+// Variadic Parameters
+func foo(x ...int) {
+	fmt.Println(x)
+	// Transforms all the things to a slice
 }
 
-// Paramters defined below
-func bar(s string) {
-	fmt.Println("hello", s)
-}
-
-// Retrun types are defined below
-func woo(s string) string {
-	return "Wooo"
-}
-
-// Retrun types are defined below
-// Different return type , match the funtion signature
-func test(fn string, ln string) (string, bool) {
-	fmt.Println("this is from te test fucntion")
-	return fn, true
-}
 func main() {
 	fmt.Println("this is for functions")
-	foo()
-	bar("Kevin")
-	s := woo("ALALAL")
-	fmt.Println(s)
-	x, y := test("Vlad", "Rodmanavich")
-	fmt.Println(x)
-	fmt.Println(y)
+	// foo()
+	// bar("Kevin")
+	// s := woo("ALALAL")
+	// fmt.Println(s)
+	// // Use short declaration operator within a fucntion body
+	// x, y := test("Vlad", "Rodmanavich")
+	// fmt.Println(x)
+	// fmt.Println(y)
+	foo(1,2,3,4,5,4,4,4,45)
 }
