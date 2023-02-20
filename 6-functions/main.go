30601374c3705fd75440446f2d78c7423783de14
kaustavhalder <khalder.08@gmail.com>
2023-02-19 19:40:17 -0600

    interfaces and polymorphism

--- 6-functions/main.go
+++ 6-functions/main.go
@@ -70,6 +70,20 @@ import "fmt"
 // 	fmt.Println("Hello my name is ", s.first, " ", s.last)
 // }
 
+
+// Interfaces and polymorphism
+// Interfaces help us to define behaviour
+
+type person struct{
+	first string
+	last string
+}
+
+type secretAgent struct{
+	person
+	ltk bool
+}
+
 func main() {
 	fmt.Println("this is for functions")
 	// foo()
