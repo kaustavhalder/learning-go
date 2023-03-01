9e0b6640c9b9ecb3122706bbe8189620d6886de4
kaustavhalder <khalder.08@gmail.com>
2023-02-28 19:32:11 -0600

    added func expression

--- 6-functions/main.go
+++ 6-functions/main.go
@@ -16,8 +16,25 @@ func main() {
 	// }
 	// fmt.Println(sa)
 
+	// Function Expression
 	f := func() {
 		fmt.Println("this is an function expression")
 	}
 	f()
+	foo()
+	bar()
+
+}
+
+// Returning a string
+func foo() string {
+	s := "hello world"
+	return s
+}
+
+// Returning a function
+func bar() func() int {
+	return func() int {
+		return 451
+	}
 }
